b76a944d66e60e8c72d737eecbde57c1046aca1d
viki <viki@c3po>
2017-02-18 16:50:44 -0600

    unit tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
+#include "PIDController.h"
 
-TEST(dummy, should_pass)
+
+TEST(PIDControllerTest, testPIDControllerCompute)
 {
-    EXPECT_EQ(1, 1);
+	PIDController p = PIDController(1,2,.4,5,.3,2);
+
+    EXPECT_EQ(1, p.compute(12.0, .4));
+    EXPECT_GT(0, p.compute(.45, 10));
+    EXPECT_LT(1, p.compute(.45, .12))
 }
